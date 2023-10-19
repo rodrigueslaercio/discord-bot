7e638e1fbc6c507df9d89acc17a4b44f5a74153c
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2023-10-19 00:08:51 -0300

    change the commands main language to pt-br

--- cogs/user_cmd.py
+++ cogs/user_cmd.py
@@ -13,10 +13,10 @@ class User(commands.Cog):
         user = ctx.message.author if user == None else user
 
         avatar_url = user.avatar
-        embed = discord.Embed(title=f"Avatar of {user.name}")
+        embed = discord.Embed(title=f"Avatar de {user.name}")
         embed.set_author(name=f"{user}", icon_url=f"{avatar_url}")
         embed.set_image(url=avatar_url)
-        embed.set_footer(text=f"Requested by: {ctx.message.author}", icon_url=ctx.author.avatar)
+        embed.set_footer(text=f"Requerido por: {ctx.message.author}", icon_url=ctx.author.avatar)
         await ctx.reply(embed=embed)
 
     @commands.hybrid_command()
@@ -24,13 +24,13 @@ class User(commands.Cog):
     async def user_info(self, ctx, *, user: discord.User=None):
         user = ctx.message.author if user == None else user 
 
-        embed = discord.Embed(title=f"Info of {user.name}")
+        embed = discord.Embed(title=f"Info de {user.name}")
         embed.set_thumbnail(url=user.avatar)
         embed.set_author(name=f"{user}", icon_url=user.avatar)
         embed.add_field(name="ID", value=user.id, inline=True)
         embed.add_field(name='Username', value=f"@{user.name}", inline=True)
-        embed.add_field(name='Global name', value=user.global_name, inline=True)  
-        embed.add_field(name="Created at", value=user.created_at, inline=True)
+        embed.add_field(name='Nome global', value=user.global_name, inline=True)  
+        embed.add_field(name="Criado em", value=user.created_at, inline=True)
         await ctx.reply(embed=embed)
     
 
